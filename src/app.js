bdec08289a71cb4eaa2a406f7e669977d3678110
Ocknyer <kdu4045@naver.com>
2022-12-05 13:49:35 +0900

    add: webpack 설정 추가

--- src/app.js
+++ src/app.js
@@ -3,8 +3,20 @@ import './style.css'
 import android from './android.png';
 import tiger from './tiger.png'
 
-document.addEventListener('DOMContentLoaded', () => {
-    document.body.innerHTML = `<img src="${android}"/><img src="${tiger}"/>`;
-})
+// document.addEventListener('DOMContentLoaded', () => {
+//     document.body.innerHTML = `<img src="${android}"/><img src="${tiger}"/>`;
+// })
 
 console.log(plus(3, 7));
+
+console.log(process.env.NODE_ENV)
+
+let env;
+if (process.env.NODE_ENV === 'development') {
+    env = dev;
+} else {
+    env = pro;
+}
+
+console.log(env);
+console.log(pro);
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
 const path = require('path');
 const webpack = require('webpack');
+const childProcess = require('child_process');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+require('dotenv').config();
 
 module.exports = {
     mode: 'development',
@@ -32,15 +36,27 @@ module.exports = {
                 test: /\.(png|jpg|gif|svg)$/,
                 type: 'asset/inline'
             },
-            
-                
-            
         ]
     },
     plugins: [
         new webpack.BannerPlugin({
                 //toLocaleString : 날짜의 문자열 표현을 지역의 언어에 맞는 형식으로 반환합니다.
-            banner: '마지막 빌드 시간은 ' + new Date().toLocaleString() + ' 입니다.'
-        })
+            banner: `
+                Commit version : ${childProcess.execSync('git rev-parse --short HEAD')}
+                Committer : ${childProcess.execSync('git config user.name')}
+                Commit Date : ${new Date().toLocaleString()}
+            `
+        }),
+        new webpack.DefinePlugin({            
+            pw: 123456
+        }),
+        new webpack.DefinePlugin({
+            dev: JSON.stringify(process.env.DEV_API),
+            pro: JSON.stringify(process.env.PRO_API)
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/index.html', // 목표 html 파일의 위치입니다.
+        }),
+        new CleanWebpackPlugin()
     ]
 }
